8ed2ab8a1fe7b9644ce4e63b6c458644276c9da7
Heza GÖLCÜK <h.golcuk91@outlook.com>
2024-12-17 22:06:16 +0300

    react homework-04 hatalar çözüldü

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,11 +1,12 @@
 import styles from './ImageCard.module.css';
 const ImageCard = ({ image, onImageClick }) => {
   return (
-    <div className={styles.imageCard} onClick={() => onImageClick(image)}>
+    <div className={styles.imageCard} >
       <img
         className={styles.image}
-        src={image.urls.regular}
+        src={image.urls.small}
         alt={image.alt_description || 'Description not available'}
+        onClick={() => onImageClick(image)}
       />
     </div>
   );
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,23 +1,28 @@
+import Modal from 'react-modal';
 import styles from './ImageModal.module.css';
+
+Modal.setAppElement('#root');
 
 const ImageModal = ({ isOpen, image, onClose }) => {
   if (!isOpen) {
     return null;
   }
   return (
-    <div className={styles.ImageModal} onClick={onClose}>
-      <div
-        className={styles.ImageModalContent}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <img
-          className={styles.modalImage}
-          src={image.urls.regular}
-          alt={image.alt_description || 'Description not available'}
-        />
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Image Modal"
+      className={styles.ImageModal}
+      overlayClassName={styles.ImageModalOverlay}
+    >
+      <div className={styles.ImageModalContent} onClick={onClose}>
+        <img className={styles.modalImage} src={image.urls.regular} alt={image.alt_description || 'Description not available'} />
       </div>
-    </div>
+    </Modal>
   );
 };
 
 export default ImageModal;
+
+
+
